f9df106f56545cc65b46d280a8071b3388a5c3a2
medendale <medendale@gmail.com>
2024-03-20 19:38:44 -0400

    Update User

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,6 @@
 export type CreateUserParams = {
     clerkId: string
     firstName: string
-    lastName: string
     username: string
     email: string
     photo: string
@@ -10,7 +9,6 @@ export type CreateUserParams = {
   
   export type UpdateUserParams = {
     firstName: string
-    lastName: string
     username: string
     photo: string
   }
