0fd14fd6612281ae5242f58ab71ad9a06c871704
medendale <medendale@gmail.com>
2024-03-20 19:33:01 -0400

    Updated User

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,14 +55,13 @@ export async function POST(req: Request) {
   const eventType = evt.type;
  
   if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-      lastName: last_name,
       photo: image_url,
     }
 
@@ -80,11 +79,10 @@ export async function POST(req: Request) {
   }
 
   if (eventType === 'user.updated') {
-    const {id, image_url, first_name, last_name, username } = evt.data
+    const {id, image_url, first_name, username } = evt.data
 
     const user = {
       firstName: first_name,
-      lastName: last_name,
       username: username!,
       photo: image_url,
     }
