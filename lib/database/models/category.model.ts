851570074f2a866d54d9e91b15ec9dd867839410
medendale <medendale@gmail.com>
2024-03-20 15:31:34 -0400

    Error Fix

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,15 +1,14 @@
-import { Document,Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
-export interface Icategory  extends Document {
-    id: string;
-    name: string;
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
 }
 
 const CategorySchema = new Schema({
-    name: { type: String, required: true unique: true },
+  name: { type: String, required: true, unique: true },
+})
 
-});
-
-const Category = models.category || model{'category', CategorySchema};
+const Category = models.Category || model('Category', CategorySchema);
 
 export default Category;
