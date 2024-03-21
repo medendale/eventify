dd35f26cebd915f4414cfd1f52df1a8d89208208
medendale <medendale@gmail.com>
2024-03-21 19:25:30 -0400

    Fixed User

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -38,68 +38,68 @@ export async function POST(req: Request) {
  
   // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent
-  } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400
+    evt = wh.verify(body, {"svix-id": svix_id,
+    "svix-timestamp": svix_timestamp,
+    "svix-signature": svix_signature,
+  }) as WebhookEvent
+} catch (err) {
+  console.error('Error verifying webhook:', err);
+  return new Response('Error occured', {
+    status: 400
+  })
+}
+
+// Get the ID and type
+const { id } = evt.data;
+const eventType = evt.type;
+
+if(eventType === 'user.created') {
+  const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+  const user = {
+    clerkId: id,
+    email: email_addresses[0].email_address,
+    username: username!,
+    firstName: first_name,
+    lastName: last_name,
+    photo: image_url,
+  }
+
+  const newUser = await createUser(user);
+
+  if(newUser) {
+    await clerkClient.users.updateUserMetadata(id, {
+      publicMetadata: {
+        userId: newUser._id
+      }
     })
   }
- 
-  // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
- 
-  if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, username } = evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      photo: image_url,
-    }
-
-    const newUser = await createUser(user);
-
-    if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id
-        }
-      })
-    }
-
-    return NextResponse.json({ message: 'OK', user: newUser })
-  }
-
-  if (eventType === 'user.updated') {
-    const {id, image_url, first_name, username } = evt.data
-
-    const user = {
-      firstName: first_name,
-      username: username!,
-      photo: image_url,
-    }
-
-    const updatedUser = await updateUser(id, user)
-
-    return NextResponse.json({ message: 'OK', user: updatedUser })
-  }
-
-  if (eventType === 'user.deleted') {
-    const { id } = evt.data
-
-    const deletedUser = await deleteUser(id!)
-
-    return NextResponse.json({ message: 'OK', user: deletedUser })
-  }
- 
-  return new Response('', { status: 200 })
+  return NextResponse.json({ message: 'OK', user: newUser })
 }
- 
+
+if (eventType === 'user.updated') {
+  const {id, image_url, first_name, last_name, username } = evt.data
+
+  const user = {
+    firstName: first_name,
+    lastName: last_name,
+    username: username!,
+    photo: image_url,
+  }
+
+  const updatedUser = await updateUser(id, user)
+
+  return NextResponse.json({ message: 'OK', user: updatedUser })
+}
+
+if (eventType === 'user.deleted') {
+  const { id } = evt.data
+
+  const deletedUser = await deleteUser(id!)
+
+  return NextResponse.json({ message: 'OK', user: deletedUser })
+}
+
+return new Response('', { status: 200 })
+}
